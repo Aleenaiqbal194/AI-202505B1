0ec0e9bb3a4725ce7e364ddaf5fa74a0b42cd6b2
afifazia <afifazia07@gmail.com>
2025-08-08 23:01:15 -0700

    operators

---
+++ JS/app.js
@@ -0,0 +1,36 @@
+//Arithmetics operator
+//let num1 = +prompt("Enter first number")
+//let num2 = +prompt("Enter second number")
+
+//let result = num1 + num2;
+//let result = num1 - num2;
+//let result = num1 * num2;
+//let result = num1 / num2;
+//let result = num1 % num2;
+
+
+
+//document.getElementById("para").innerHTML = result;
+
+//increament decrement operator
+
+//let a = 8;
+//let b = 5;
+//let result = a++ + b + ++a + a + --b + a++
+//alert(result)
+//document.write(result)
+
+//let firstName; //declare
+//firstName + "aptech"
+
+
+//Relational Operators
+//==, ===, !=, !==, <, >, <=, >=
+
+let a = prompt("Enter first number")
+let b = prompt("Enter second name")
+
+//document.write(typeof(a))
+ 
+let result = a > b
+document.write(result)
