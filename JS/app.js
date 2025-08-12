0753a0d6b8309faf3497b808706d5e8d9813108b
afifazia <afifazia07@gmail.com>
2025-08-11 22:58:53 -0700

    assests

--- JS/app.js
+++ JS/app.js
@@ -27,10 +27,39 @@
 //Relational Operators
 //==, ===, !=, !==, <, >, <=, >=
 
-let a = prompt("Enter first number")
-let b = prompt("Enter second name")
+// let a = prompt("Enter first number")
+// let b = prompt("Enter second name")
 
 //document.write(typeof(a))
  
-let result = a > b
-document.write(result)
+// let result = a > b
+// document.write(result)
+
+//  logical operators
+// AND && NOT ! OR ||
+//  let num1 = 20;
+//  let num2 = 30;
+  
+//  let result = num1 <= num2 || num1 == num2
+//  document.getElementById("result").innerHTML = result
+
+//   let num1 = 20;
+//   let num2 = 30;
+//   let num3 = 10;
+
+
+//   let result = !(num1 <= num2 && num1 != num2 && num3 > num1)
+//   document.getElementById("result").innerHTML = result
+
+// Assignment Operator
+// =, +=, -=, *=, /=, %=
+let a = 20;
+let b = 10;
+
+a += b 
+a -= b
+a *= b
+a /= b
+ document.write(a)
+ 
+
---
+++ JS/assests/app.js
@@ -0,0 +1,19 @@
+// #if else
+//      let name = "aptech sfc"
+
+// if(name == "aptech sfc"){
+//     document.write("welcome to aptech sfc")
+
+// }
+// else{
+//     document.write("go to aptech msg")
+// }
+
+let num = -68
+if(num > 0){
+    document.write("this is positive number")
+}
+ else{
+    document.write("this is negative number")
+
+ }
