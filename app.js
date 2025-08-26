de70917497ef1621c73db0fdcec31eee50994eb1
Aleenaiqbal194 <aleenaiqbal19409@gmail.com>
2025-08-25 23:04:41 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// #if else
+//      let name = "aptech sfc"
+
+// if(name == "aptech sfc"){
+//     document.write("welcome to aptech sfc")
+
+// }
+// else{
+//     document.write("go to aptech msg")
+// }
+
+// let num = -68
+// if(num > 0){
+//     document.write("this is positive number")
+// }
+//  else{
+//     document.write("this is negative number")
+
+//  }
+
+let student = ["ali","zain","areeb","faizan","hasan"]
+document.write(student[2],"&nbsp;",student[4], "<br>")
+console.log(student)
+
+
+let courses = new Array("CPISM","DISM","HDSE1","HDSE2" )
+document.write(courses[3], " <Br>")
+console.log(courses)
+document.write(student.length)
---
+++ loop.js
@@ -0,0 +1,23 @@
+// -----------------------------------while loop---------------------------------------
+// let a = 0;
+// while (a <=10){
+//     document.write(a,"<br>")
+//     a += 2
+// }
+
+// --------------------do while-------------------------
+// let a = 0;
+// do{
+//     document.write(a, " <br>")
+//     a += 2
+// }while(a <=10)
+ 
+
+let a = 0;
+do{
+    if(a == 7){
+      break;
+    }
+    document.write(a,"<br>")
+    a++
+}while(a <=10)
