4d679f66d7b76674b4bc81f4ffef4954aef74f23
Aleenaiqbal194 <aleenaiqbal19409@gmail.com>
2025-08-27 22:55:39 -0700

    Add files via upload

---
+++ multiarray.js
@@ -0,0 +1,20 @@
+// multidimension Array
+ let courses = [
+    ["HTML","CSS","JS","Bootstrap" , "JQuery"],
+    ["XML","Json","Adv.Js","Mysql","PHP"],
+    ["SQL SERVER","C#",".Net","Angular"],
+]
+//  console.log(courses)
+//  document.write(courses[0][3], "<br>" ,courses[2][2])
+  
+// document.write(courses)
+// for(let value of courses){
+//     document.write(value[1], "<br>" ,value[0], "<br>")
+// }
+
+// for(value of courses){
+
+//     for(data of value){
+//         document.write(data,"<br>")
+//     }
+// }
